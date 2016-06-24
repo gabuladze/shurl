dea5c5d1c4130fa0f3ca4042cb30e0675f0ea6ca
Giorgi Abuladze <g.abuladze@live.com>
2016-06-24 22:00:16 +0400

    Cleaned up Code

--- app/api/urlShortener.js
+++ app/api/urlShortener.js
@@ -19,13 +19,15 @@ module.exports = function(app, db) {
         { _id: 0 },
         function(err, result) {
           if (result) {
-            res.send(result);
+            // Send from db
+            res.json(result);
           } else {
+            // Save new document in db
             var urlObj = {
               "original_url" : url,
               "short_url" : createShortUrl(url)
             };
-            res.send(urlObj);
+            res.json(urlObj);
             saveUrl(urlObj, db);
           }
         });
@@ -72,6 +74,7 @@ module.exports = function(app, db) {
   }
 
   function validateUrl(url) {
+    // Regex from https://gist.github.com/dperini/729294
     var regex = /^(?:(?:https?|http):\/\/)(?:\S+(?::\S*)?@)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,}))\.?)(?::\d{2,5})?(?:[/?#]\S*)?$/i;
     return regex.test(url);
   }
